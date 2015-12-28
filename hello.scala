3485449cb91473bc79c0647e471d7a67aefae742
Areg <areg@localhost.localdomain>
2015-12-28 22:44:31 +0400

    Add hello.scala fail

---
+++ hello.scala
@@ -0,0 +1,13 @@
+/**
+ * object: Singlton class
+ * def: Funkciayi tarberanshan
+ * main glxavor, skzbnakan funkcia cragri skizb
+ * arg: Array[String] untunman argumentnneri cucak voronq toxer en
+ * = println("Hello") funkciayi marmin = partadir e 
+ * ete 1 toxic avele petqe nerarel {} kam vorosh depqerum ; anjatel
+ * scalac hello.scala compile
+ * scala Hello run
+ **/
+object Hello {
+	def main(arg: Array[String]) = println("Hello")
+}
